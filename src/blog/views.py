fe091fb41d06b2a83d1c316812148077080fa58d
Gregory Alpizar <gregory.alce2001@gmail.com>
2023-08-18 00:19:52 -0600

    Create all blog views

--- src/blog/views.py
+++ src/blog/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def home(request):
+    return render(request, 'blog/index.html')
+
+
+def generales(request):
+    return render(request, 'blog/generales.html')
+
+
+def programacion(request):
+    return render(request, 'blog/programacion.html')
+
+
+def tutoriales(request):
+    return render(request, 'blog/tutoriales.html')
+
+
+def tecnologia(request):
+    return render(request, 'blog/tecnologia.html')
+
+
+def videojuegos(request):
+    return render(request, 'blog/videojuegos.html')
