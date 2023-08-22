aef776e4fd6b9d0e1a4dc19cc5687f855eeabd81
Gregory Alpizar <gregory.alce2001@gmail.com>
2023-08-21 23:59:08 -0600

    Showing posts list on index and filtered by categories

--- src/blog/views.py
+++ src/blog/views.py
@@ -1,25 +1,38 @@
 from django.shortcuts import render
+from blog.models import Post, Categoria
 
 
 def home(request):
-    return render(request, 'blog/index.html')
+    posts = Post.objects.filter(estado=True)
+
+    return render(request, 'blog/index.html', {'posts': posts})
 
 
 def generales(request):
-    return render(request, 'blog/generales.html')
+    posts = Post.objects.filter(estado=True, categoria=Categoria.objects.get(nombre='General'))
+
+    return render(request, 'blog/generales.html', {'posts': posts})
 
 
 def programacion(request):
-    return render(request, 'blog/programacion.html')
+    posts = Post.objects.filter(estado=True, categoria=Categoria.objects.get(nombre='Programacion'))
+
+    return render(request, 'blog/programacion.html', {'posts': posts})
 
 
 def tutoriales(request):
-    return render(request, 'blog/tutoriales.html')
+    posts = Post.objects.filter(estado=True, categoria=Categoria.objects.get(nombre='Tutoriales'))
+
+    return render(request, 'blog/tutoriales.html', {'posts': posts})
 
 
 def tecnologia(request):
-    return render(request, 'blog/tecnologia.html')
+    posts = Post.objects.filter(estado=True, categoria=Categoria.objects.get(nombre='Tecnologia'))
+
+    return render(request, 'blog/tecnologia.html', {'posts': posts})
 
 
 def videojuegos(request):
-    return render(request, 'blog/videojuegos.html')
+    posts = Post.objects.filter(estado=True, categoria=Categoria.objects.get(nombre='Video juegos'))
+
+    return render(request, 'blog/videojuegos.html', {'posts': posts})
